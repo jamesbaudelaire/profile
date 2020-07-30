feee877809d8429c312d47ece1a734f48c2a3e79
Ronak Mistry <androidmystery7@gmail.com>
2020-07-29 19:56:13 -0500

    x

--- src/components/images.js
+++ src/components/images.js
@@ -7,7 +7,7 @@ export const Images = ({ project }) => {
         <img
           key={n}
           alt={`${project.id}-screen-${n}`}
-          src={`https://res.cloudinary.com/baudelaire/image/upload/w_500/v1595983277/portfolio/${
+          src={`https://res.cloudinary.com/baudelaire/image/upload/w_500/portfolio/${
             project.id
           }-${n}.png`}
         />
--- src/ronak.js
+++ src/ronak.js
@@ -13,13 +13,11 @@ export const Ronak = {
       links: [
         {
           icon: "link",
-          link: `https://rsrv.netlify.app`,
-          name: "URL"
+          link: `https://rsrv.netlify.app`
         },
         {
           icon: "code",
-          link: `https://github.com/jamesbaudelaire/reserve`,
-          name: "GITHUB"
+          link: `https://github.com/jamesbaudelaire/reserve`
         }
       ],
       features: [
@@ -41,13 +39,11 @@ export const Ronak = {
       links: [
         {
           icon: "link",
-          link: `https://m3nu.netlify.app`,
-          name: "URL"
+          link: `https://m3nu.netlify.app`
         },
         {
           icon: "code",
-          link: `https://github.com/jamesbaudelaire/menu`,
-          name: "GITHUB"
+          link: `https://github.com/jamesbaudelaire/menu`
         }
       ],
       features: [
@@ -56,6 +52,27 @@ export const Ronak = {
         "filter by type",
         "URL routing"
       ]
+    },
+    {
+      name: "POKEDEX",
+      id: "pokedex",
+      url: `https://pokedex-v2.netlify.app/`,
+      tools: [
+        "Cloudinary for image hosting",
+        "Netlify and Github for CI/CD",
+        "React"
+      ],
+      links: [
+        {
+          icon: "link",
+          link: `https://pokedex-v2.netlify.app/`
+        },
+        {
+          icon: "code",
+          link: `https://github.com/jamesbaudelaire/pokedex`
+        }
+      ],
+      features: ["filter search", "size comparison"]
     }
   ],
   name: "RONAK MISTRY",
