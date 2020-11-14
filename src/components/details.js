bc98742c7cde484a162b147ec1896ce01e5328f4
Ronak Mistry <androidmystery7@gmail.com>
2020-11-14 10:59:57 -0600

    x

--- src/components/details.js
+++ src/components/details.js
@@ -58,7 +58,12 @@ export const Details = ({ project, selectProject }) => {
         <span className="title">LINKS</span>
         <div>
           {project.links.map(({ icon, link, name }) => (
-            <a href={link} key={icon} target="_blank" rel="noopener noreferrer">
+            <a
+              href={`https://github.com/jamesbaudelaire/${link}`}
+              key={icon}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <div>
                 <i className="material-icons-round link">{icon}</i>
               </div>
